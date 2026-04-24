7bb28c81200c91d89d553f66226fdb827b0d4686
Koyalkar Pavan Sai <pavansaikoyalkar59@gmail.com>
2026-04-25 00:10:02 +0530

    Message (Ctrl+Enter to commit)

---
+++ StringLengthAndCaseExample.js
@@ -0,0 +1,15 @@
+const myName = "Pavan";
+console.log(myName[0]); // Output: P
+console.log(myName[1]); // Output: a
+console.log(myName[4]); // Output: n
+
+console.log(myName.length); // Output: 5
+
+const upperCasemyName= myName.toUpperCase();
+console.log('Upper Case My Name is',upperCasemyName); // Output: PAVAN
+
+const lowerCaseMyName = myName.toLowerCase();
+console.log('Lower Case My Name is',lowerCaseMyName); // Output: pavan
+
+
+console.log('myName is',myName); // Output: Pavan;
